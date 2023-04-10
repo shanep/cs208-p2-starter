615c4699d86226194385dcc6bed187e8d600b188
Shane Panter <shanepanter@boisestate.edu>
2023-04-10 16:09:07 -0600

    Stub out files

---
+++ test/test.js
@@ -0,0 +1,12 @@
+import test from 'node:test';
+import assert from 'node:assert/strict';
+import {JSDOM} from 'jsdom';
+import fs from 'node:fs';
+
+
+const file = fs.readFileSync('index.html', {encoding: 'utf8'});
+const dom = new JSDOM(file);
+
+/**
+ * DO NOT MODIFY THIS FILE!
+ */
